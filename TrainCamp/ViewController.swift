f938d4839a97a0dd1c4c8428b414b35a952d800b
Nigar Azadaliyev <nigar.azadaliyeva@gmail.com>
2022-06-08 11:21:17 +0200

    Initial Commit

---
+++ TrainCamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainCamp
+//
+//  Created by user216966 on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
