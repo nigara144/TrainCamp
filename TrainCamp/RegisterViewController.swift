fa7f11a5a516933c420fe10a07a91653f04544e8
hod <hod.gohasi@hotmail.com>
2022-06-10 16:25:48 +0300

    * Created - sign up screen

---
+++ TrainCamp/RegisterViewController.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+//import FirebaseAuth
+import FirebaseAuthUI
+//import FirebaseUI
+
+class RegisterViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+       
+    }
+    
+}
